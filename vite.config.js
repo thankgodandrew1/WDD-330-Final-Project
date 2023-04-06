4e9e583aff4de501d1b5dad9e1d9521d877bd1bc
thankgodandrew1 <and22046@byui.edu>
2023-04-06 05:32:35 -0600

    update on vite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "src/",
+  root: 'src/',
 
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        weatherMap: resolve(__dirname, "src/interactive-weather-map.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        weatherMap: resolve(__dirname, 'src/interactive-weather-map.html'),
         windChillCalculator: resolve(
           __dirname,
-          "src/wind-chill-calculator.html"
+          'src/wind-chill-calculator.html'
         ),
-        aboutUs: resolve(__dirname, "src/about.html"),
-        privacyPolicy: resolve(__dirname, "src/privacy-policy.html"),
-        weatherCommunity: resolve(__dirname, "src/weather-community.html"),
+        aboutUs: resolve(__dirname, 'src/about.html'),
+        privacyPolicy: resolve(__dirname, 'src/privacy-policy.html'),
+        weatherCommunity: resolve(__dirname, 'src/weather-community.html'),
       },
     },
   },
